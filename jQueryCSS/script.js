6f57f9ea509988843b26fa219e6c55fb31c3ee7d
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-08-06 18:16:34 +0000

    Initial commit

---
+++ jQueryCSS/script.js
@@ -0,0 +1,10 @@
+/*global $*/
+
+//selects all divs
+$('div').css('background-color', 'purple');
+//selects class highlight
+$('.highlight').css('width', '200px');
+//selects id third
+$('#third').css('border', '2px solid orange');
+//selects first div first-of-type is a CSS selector
+$('div:first-of-type').css('color', 'pink');
